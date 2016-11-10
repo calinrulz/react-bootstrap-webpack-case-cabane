8e4d985f3528bbbb25dce4ea9db1e8dee660961f
calinrulz <calinrulz@gmail.com>
2016-11-10 22:12:14 +0200

    Added  2Tabs indentention

--- resources/jsx/HelloMessage.jsx
+++ resources/jsx/HelloMessage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 class HelloMessage extends React.Component {
-
-    render() {
-        return (
-            <div className="jumbotron">
-                <h1>Hello, {this.props.name}!</h1>
-                <p>Boilerplate is ready to develop!</p>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Hello, {this.props.name}!</h1>
+        <p>Boilerplate is ready to develop!</p>
+      </div>
+    );
+  }
 };
 
 export default HelloMessage;
--- server.js
+++ server.js
@@ -3,16 +3,15 @@ var webpack = require('webpack');
 var config = require('./webpack.config');
 
 var server = new WebpackDevServer(webpack(config), {
-    publicPath: 'http://localhost:8080/',
-    contentBase: './app',
-    hot: true,
-    noInfo: true
+  publicPath: 'http://localhost:8080/',
+  contentBase: './app',
+  hot: true,
+  noInfo: true
 });
 
 server.listen(8080, 'localhost', function(err) {
-    if (err) {
-        console.log(err);
-    }
-
-    console.log('Listening at localhost:8080');
+  if (err) {
+    console.log(err);
+  }
+  console.log('Listening at localhost:8080');
 });
