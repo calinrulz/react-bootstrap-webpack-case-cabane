c3b284063f178faf7caa40d888e86be5f7aca47f
calinrulz <calinrulz@gmail.com>
2016-11-10 22:33:39 +0200

    Added Bootstrap JavaScript and jQuery

--- resources/index.js
+++ resources/index.js
@@ -2,6 +2,9 @@
 require('bootstrap/dist/css/bootstrap');
 require('bootstrap/dist/css/bootstrap-theme');
 
+// Bootstrap JSs
+require('bootstrap/dist/js/bootstrap');
+
 // App styles in Less
 require('assets/less/app');
 
