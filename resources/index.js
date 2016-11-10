09b2a76b49236cb0e718f602dd46357965d35677
calinrulz <calinrulz@gmail.com>
2016-11-10 22:38:21 +0200

    Removed extra letter in comment

--- resources/index.js
+++ resources/index.js
@@ -2,7 +2,7 @@
 require('bootstrap/dist/css/bootstrap');
 require('bootstrap/dist/css/bootstrap-theme');
 
-// Bootstrap JSs
+// Bootstrap JS
 require('bootstrap/dist/js/bootstrap');
 
 // App styles in Less
