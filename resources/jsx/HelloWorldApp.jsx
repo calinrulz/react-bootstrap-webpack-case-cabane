788fe189f0b992be3b2b93e5264dfe75ba3a8aa3
calinrulz <calinrulz@gmail.com>
2016-11-11 11:06:18 +0200

    Changed name from Hello, World to Hello, Gabi

--- resources/jsx/HelloWorldApp.jsx
+++ resources/jsx/HelloWorldApp.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import HelloMessage from './HelloMessage';
 
-React.render(<HelloMessage name="World" />, document.getElementById('content'));
+React.render(<HelloMessage name="Gabi" />, document.getElementById('content'));
