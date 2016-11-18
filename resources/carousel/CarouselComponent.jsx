fe320b98931da900efa69e7be2fa3243a1f87500
calinrulz <calinrulz@gmail.com>
2016-11-18 13:36:41 +0200

    Added Carousel in place and added some dummy image in place

--- resources/carousel/CarouselComponent.jsx
+++ resources/carousel/CarouselComponent.jsx
@@ -3,39 +3,39 @@ import React from 'react';
 class Carousel extends React.Component {
   render () {
     return (
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        // Indicators
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-
-        // Wrapper for slides
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img src="..." alt="...">
-            <div class="carousel-caption">
+      <div id="carousel-example-generic" className="carousel slide cabane-main-slider" data-ride="carousel">
+        {/* Wrapper for slides */}
+        <div className="carousel-inner" role="listbox">
+          <div className="item active">
+            <img src="http://www.planwallpaper.com/static/images/beautiful-sunset-images-196063.jpg" alt="..." />
+            <div className="carousel-caption">
               ...
             </div>
           </div>
-          <div class="item">
-            <img src="..." alt="...">
-            <div class="carousel-caption">
+          <div className="item">
+            <img src="http://www.planwallpaper.com/static/images/beautiful-sunset-images-196063.jpg" alt="..." />
+            <div className="carousel-caption">
               ...
             </div>
           </div>
         </div>
 
-        // Controls
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+        {/* Controls */}
+        <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+        <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
         </a>
+
+        {/* Indicators */}
+        <ol className="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
       </div>
     );
   }
