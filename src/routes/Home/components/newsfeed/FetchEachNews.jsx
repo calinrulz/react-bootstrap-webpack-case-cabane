8c25799bbee590856143cfab7c887680b9320901
Calin Georgescu <calgeorg@cisco.com>
2017-08-08 13:16:27 +0300

    Tested adding image paths from firebase

--- src/routes/Home/components/newsfeed/FetchEachNews.jsx
+++ src/routes/Home/components/newsfeed/FetchEachNews.jsx
@@ -15,20 +15,41 @@ class FetchEachComponent extends React.Component {
   componentDidMount() {
     const rootRef = firebase.database().ref()
     const speedRef = rootRef.child('speed')
+    const imageRef = rootRef.child('imageRef').child('url')
 
-    speedRef.on('value', snap => {
+    speedRef.on('value', (snap) => {
       console.log(snap.val())
-      
+
       this.setState({
         speed: snap.val()
+      })
+    })
+
+    imageRef.on('value', (snap) => {
+      console.log(snap.val())
+
+      this.setState({
+        imagePath: snap.val()
       })
     })
   }
 
   render () {
-    return(
+    return (
       <div>
         <h1>{this.state.speed}</h1>
+
+        <div>
+          { this.state.imagePath &&
+            Object.keys(this.state.imagePath).map((key, index) => {
+              return (
+                <div key={index}>
+                  <img src={this.state.imagePath[key]} alt="" className="uploaded-image" />
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
